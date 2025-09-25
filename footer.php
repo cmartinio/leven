9b5bd4790086bc43876b9b1b2eea53261dd883d6
christian Martin <cmartn@pm.me>
2025-09-26 01:19:49 +0200

    Still working on fixing the repo

---
+++ footer.php
@@ -0,0 +1,46 @@
+<?php 
+$footer_crs = ( function_exists( 'fw_get_db_settings_option' ) ) ? fw_get_db_settings_option('footer_copyrights') : '';
+$footer_links = ( function_exists('fw_get_db_settings_option') ) ? fw_get_db_settings_option('footer_social') : '';
+$show_blog_sidebar = ( function_exists('fw_get_db_settings_option') ) ? fw_get_db_settings_option('blog_sidebar') :  'yes';
+?>  
+            <?php if ( !empty($footer_links) || !empty($footer_crs) ) { ?>
+            <footer class="site-footer clearfix">
+                <?php if ( !empty($footer_links)) { ?>
+                <div class="footer-social">
+                    <ul class="footer-social-links">
+                    <?php
+                    foreach ($footer_links as $footer_links):
+                        if( !empty( $footer_links['link'] ) ) :
+                        $target = (!isset($footer_links['target'])) ? '_blank' : $footer_links['target'];
+                    ?>
+                        <li>
+                            <a href="<?php echo esc_url($footer_links['link']); ?>" target="<?php echo esc_attr($target) ?>"><?php echo wp_kses_post($footer_links['title']); ?></a>
+                        </li>
+                    <?php endif;
+                    endforeach;
+                    ?>
+                    </ul>
+                </div>
+                <?php } ?>
+
+                <?php if (!empty($footer_crs)) { ?>
+                <div class="footer-copyrights">
+                    <p><?php echo wp_kses_post($footer_crs); ?></p>
+                </div>
+                <div class="footer-contact">
+                    <a href="mailto:contact@cmartn.com"><i class="fa fa-shield"></i> contact@cmartn.com</a><a href="https://keys.openpgp.org/search?q=cmartn%40proton.me" class="pgp-key-link" target="_blank"><i class="fa fa-key"></i> PGP</a>
+                    <a href="mailto:cmartn@pm.me"><i class="fa fa-shield"></i> cmartn@pm.me</a><a href="https://keys.openpgp.org/search?q=cmartn%40proton.me" class="pgp-key-link" target="_blank"><i class="fa fa-key"></i> PGP</a>
+                </div>
+                <?php } ?>
+            </footer>
+            <?php } ?>
+        </div>
+        <?php if ( $show_blog_sidebar == 'yes'):
+            get_sidebar( 'content' );
+        endif; ?>
+    </div>
+	<?php
+		wp_footer();
+	?>
+</body>
+</html>
